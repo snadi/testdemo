728aef026d79df3831a1958cd704555b398f43a2
Sarah Nadi <nadi@ualberta.ca>
2024-02-16 12:23:31 +0400

    Improve exception handling and test it

--- src/main/java/nyuad/cs3216/Calculator.java
+++ src/main/java/nyuad/cs3216/Calculator.java
@@ -1,9 +1,13 @@
 package nyuad.cs3216;
 
+import nyuad.cs3216.exceptions.DivideByZeroException;
+
 public class Calculator {
    private double currentValue;
 
-   public double divide(int a, int b) {
+   public double divide(int a, int b) throws DivideByZeroException {
+
+      if (b == 0) throw new DivideByZeroException("Cannot divide by zero");
 
       double result = (double) a / (double) b;
       currentValue = result;
